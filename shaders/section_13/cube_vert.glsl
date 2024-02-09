bdffb166736ac98a9310b24a0016f965dc4020bc
MysteryCoder456 <rehatbir.singh@gmail.com>
2024-02-09 12:04:50 +0400

    surface normals take rotation into account

--- shaders/section_13/cube_vert.glsl
+++ shaders/section_13/cube_vert.glsl
@@ -13,5 +13,5 @@ uniform mat4 projection;
 void main() {
     gl_Position = projection * view * model * vec4(aPos, 1.0);
     FragPos = vec3(model * vec4(aPos, 1.0));
-    Normal = aNormal;
+    Normal = transpose(inverse(mat3(model))) * aNormal;
 }
