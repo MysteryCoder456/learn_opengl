2789b419e63f81a328cade10fb8c618f25651102
MysteryCoder456 <rehatbir.singh@gmail.com>
2024-03-13 11:43:22 +0400

    Lighting: Lighting Maps - Diffuse Maps

---
+++ shaders/section_15/cube_frag.glsl
@@ -0,0 +1,47 @@
+#version 330 core
+
+struct Material {
+    sampler2D diffuse;
+    vec3 specular;
+    float shininess;
+};
+
+struct Light {
+    vec3 position;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+in vec3 FragPos;
+in vec3 Normal;
+in vec2 TexCoord;
+
+out vec4 FragColor;
+
+uniform Material material;
+uniform Light light;
+uniform vec3 cameraPos;
+
+void main() {
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(light.position - FragPos);
+    vec3 cameraDir = normalize(cameraPos - FragPos);
+
+    vec3 diffuseTexel = vec3(texture(material.diffuse, TexCoord));
+
+    // Ambient lighting
+    vec3 ambient = light.ambient * diffuseTexel;
+
+    // Diffuse lighting
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = light.diffuse * diff * diffuseTexel;
+
+    // Specular lighting
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(reflectDir, cameraDir), 0.0), material.shininess);
+    vec3 specular = light.specular * spec * material.specular;
+
+    vec3 result = (ambient + diffuse + specular);
+    FragColor = vec4(result, 1.0);
+}
